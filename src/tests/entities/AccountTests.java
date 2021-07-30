a666f74265a16948b2db74efc65619b1d8f05fba
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-30 09:12:41 -0300

    Class 06-11

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 	
@@ -12,7 +13,7 @@ public class AccountTests {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.createEmptyAccount();
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());;
@@ -24,10 +25,22 @@ public class AccountTests {
 		
 		double amount = -10.0;
 		double expectedValue = 10.0;
-		Account acc = new Account(1L, 10.0);
+		Account acc = AccountFactory.createAccount(10.0);
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());;
+	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
 	}
 
 }
